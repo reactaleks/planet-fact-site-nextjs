5760b1afdae515bb0f9256075db2b7c96dacd96d
Aleks <a.ivanov.developer@gmail.com>
2024-07-23 13:48:31 +0100

    added planet data as json file, imported data into app

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import planetData from "../public/planetData/planetData.json";
+
 export default function Home() {
-  return <main className="min-h-screen"></main>;
+  return (
+    <>
+      <main className="min-h-screen">
+      </main>;
+    </>
+  );
 }
